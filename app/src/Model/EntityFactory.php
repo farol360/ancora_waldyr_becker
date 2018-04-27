083e8147654ffa459b6fe60de1b987dbe625482e
thnardi <thnardi@gmail.com>
2018-04-27 13:01:44 -0300

    entity object

--- app/src/Model/EntityFactory.php
+++ app/src/Model/EntityFactory.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace Farol360\Ancora\Model;
 
 // business objects
-
+use Farol360\Ancora\Model\Disease;
+use Farol360\Ancora\Model\Patient;
+use Farol360\Ancora\Model\PatientType;
 
 // Ancora objects
 use Farol360\Ancora\Model\Permission;
@@ -13,6 +15,22 @@ use Farol360\Ancora\Model\User;
 
 class EntityFactory
 {
+
+    // business classes
+    public function createDisease(array $data = []): Disease
+    {
+        return new Disease($data);
+    }
+
+    public function createPatient(array $data = []): Patient
+    {
+        return new Patient($data);
+    }
+
+    public function createPatientType(array $data = []): PatientType
+    {
+        return new PatientType($data);
+    }
 
     // permission, users and role Classes
     public function createPermission(array $data = []): Permission
