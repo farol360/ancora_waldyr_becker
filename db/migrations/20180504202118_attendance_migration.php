ecc4ceabe65fe33c1d5e4ef58ab834d232cc1923
thnardi <thnardi@gmail.com>
2018-06-24 11:32:55 -0300

    horário in add atendimento

--- db/migrations/20180504202118_attendance_migration.php
+++ db/migrations/20180504202118_attendance_migration.php
@@ -31,6 +31,7 @@ class AttendanceMigration extends AbstractMigration
         $event_logs->addColumn('id_patient', 'integer', ['null' => true]);
         $event_logs->addColumn('id_professional', 'integer', ['null' => true]);
         $event_logs->addColumn('date', 'timestamp');
+        $event_logs->addColumn('time', 'time');
         $event_logs->addColumn('description', 'string');
         $event_logs->create();
     }
